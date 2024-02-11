52935d46ed6bbd0d882970395f3de5447c2f7f95
rjlacanlaled <rjlacanlaled@gmail.com>
2024-02-12 01:37:07 +0800

    Add constants for liquidity pool NFTs and validator addresses

---
+++ onchain/poc/e2e/constants.ts
@@ -0,0 +1,21 @@
+// Liquidity Pool NFTs
+const ADA_IUSD_NFT_SPECTRUM =
+  "4e5571315812e93aa829ab30f8ca83d63efce53853c5bf58c78cb5b1695553445f4144415f4e4654";
+const ADA_USDC_NFT_SPECTRUM =
+  "6833492a271a21abcecb971f915150d9c2283d0d747b88be9cdf0b75555344435f4144415f4e4654";
+const ADA_IUSD_NFT_MINSWAP =
+  "0be55d262b29f564998ff81efe21bdc0022621c12f15af08d0f2ddb18fde43a3f0b9f0e6f63bec7335e0b855c6b62a4dc51f1b762ccb6dfbbafcfe47";
+
+// Validator Addresses
+const SPECTRUM_LP_ADDRESS =
+  "addr1x94ec3t25egvhqy2n265xfhq882jxhkknurfe9ny4rl9k6dj764lvrxdayh2ux30fl0ktuh27csgmpevdu89jlxppvrst84slu";
+const MINSWAP_LP_ADDRESS =
+  "addr1z8snz7c4974vzdpxu65ruphl3zjdvtxw8strf2c2tmqnxzdqcjdumqxjmc7cx0sg275pq4kkqx9wc99ffjc65eq9qn8q5vtgde";
+
+export {
+  ADA_IUSD_NFT_MINSWAP,
+  ADA_IUSD_NFT_SPECTRUM,
+  ADA_USDC_NFT_SPECTRUM,
+  MINSWAP_LP_ADDRESS,
+  SPECTRUM_LP_ADDRESS,
+};
